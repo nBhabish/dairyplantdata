0042e40a5c096a7f65945ea9705a91145ba2afcf
nbhabish <bhabishya.neupane@huskers.unl.edu>
2022-03-28 18:52:13 -0500

    added cleaning script

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,19 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# library -----------------------------------------------------------------
+
+library(tidyverse)
+library(janitor)
+
+# quick-clean up
+
+dairy_plant_raw_data <- read_csv("data-raw/dairyPlantProject.csv")
+
+dairy_plant_data <- dairy_plant_raw_data |> 
+  janitor::clean_names() |> 
+  mutate(across(everything(), .fns = ~str_to_lower(.))) |> 
+  select(-1)
+
+
+usethis::use_data(dairy_plant_data, overwrite = TRUE)
+
